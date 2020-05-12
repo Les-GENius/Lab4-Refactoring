36e053b5cc3986043d1648b8f1d0486d14559453
Doran Kayoumi <dorankayoumi@gmail.com>
2020-05-12 11:49:23 +0200

    bad smell: temporary variable, refactor: replace temp with query

--- src/main/java/ch/heigvd/gen/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen/OrdersWriter.java
@@ -18,9 +18,7 @@ public class OrdersWriter {
             sb.append(", ");
             sb.append("\"products\": [");
             for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-
-                getProductContent(sb, product);
+                getProductContent(sb, order.getProduct(j));
             }
 
             if (order.getProductsCount() > 0) {
