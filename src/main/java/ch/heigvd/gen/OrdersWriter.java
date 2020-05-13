fd2825ab5e4df7428437728233d23f613e25a01e
Gaetan2907 <gaetan.daubresse@gmail.com>
2020-05-13 09:12:09 +0200

    bad smell: long method, refactor: extract method getContent

--- src/main/java/ch/heigvd/gen/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen/OrdersWriter.java
@@ -1,39 +1,10 @@
 package ch.heigvd.gen;
 
 public class OrdersWriter {
-    private Orders orders;
+    Orders orders;
 
     public OrdersWriter(Orders orders) {
         this.orders = orders;
-    }
-
-    public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
-
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                sb.append(order.getProduct(j));
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
-        }
-
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        return sb.append("]}").toString();
     }
 
 }
